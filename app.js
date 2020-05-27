90fb5a6ce8308a99d435be55890ae4ed59244ddb
Kartik Kher <kartikkher777@gmail.com>
2020-05-27 14:52:09 +0100

    WIP: Delete books from list

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ UI.prototype.showAlert = function (message, className) {
   }, 3000);
 };
 
+// Delete Book
+UI.prototype.deleteBook = function (target) {
+  if (target.className === "delete") {
+    target.parentElement.parentElement.remove();
+  }
+};
+
 // Clear Fields
 UI.prototype.clearFields = function () {
   document.getElementById("title").value = "";
@@ -48,7 +55,7 @@ UI.prototype.clearFields = function () {
   document.getElementById("isbn").value = "";
 };
 
-// Event Listner
+// Event Listner add book
 document.getElementById("book-form").addEventListener("submit", function (e) {
   // Get form values
   const title = document.getElementById("title").value;
@@ -75,5 +82,16 @@ document.getElementById("book-form").addEventListener("submit", function (e) {
     ui.clearFields();
   }
 
+  e.preventDefault();
+});
+
+// Event listner for delete
+document.getElementById("book-list").addEventListener("click", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+  // Delete book
+  ui.deleteBook(e.target);
+  // Show message
+  ui.showAlert("Book Removed", "success");
   e.preventDefault();
 });
